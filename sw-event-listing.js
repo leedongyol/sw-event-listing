e00eeeba0d3589ea950d79cf95e853c87360ba52
David Pierce <david.dean.pierce@gmail.com>
2012-09-09 21:52:22 -0700

    Set up the plugin function to be called on the jQuery selector object

--- sw-event-listing.js
+++ sw-event-listing.js
@@ -8,4 +8,12 @@
     testHarness = window.testHarness;
   }
 
+  /**
+   * This plugin generates an html table based on data points
+   * provided by the SWOOP API
+   *
+   * @opts - The settings for the plugin
+   */
+  $.fn.sw-event-listing = function (opts) {
+  };
 })(jQuery);
