0e5270e1eb9d194979131f5a80f485ab6bf3c08e
David Pierce <david.dean.pierce@gmail.com>
2012-09-09 21:46:31 -0700

    Set up test framework for the plugin

--- sw-event-listing.js
+++ sw-event-listing.js
@@ -1,4 +1,11 @@
 "use strict";
 
 (function ($) {
+  // Functions and top-level declarations
+  var testHarness;
+
+  if (window.testHarness) {
+    testHarness = window.testHarness;
+  }
+
 })(jQuery);
---
+++ test.js
@@ -0,0 +1,6 @@
+var testHarness = window.testHarness || {};
+
+module('test for tests');
+test('The test module works', function () {
+  equal(1, 1);
+});
